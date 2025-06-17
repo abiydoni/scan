f77a835b940131aa07991b09438407c3834931dd
abiydoni <abiydoni@gmail.com>
2025-06-17 14:06:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -154,9 +154,7 @@ $hp_link = preg_replace('/^0/', '62', $hp);
 
   <div class="container">
   <h2>Selamat datang, <?= htmlspecialchars($_SESSION['user']['user_name']) ?></h2>
-    <?php if (isset($_SESSION['device_id'])): ?>
       <h5>Device ID anda: <?= htmlspecialchars($_SESSION['device_id']) ?></h5>
-    <?php endif; ?>
     <h3 style="color:grey;">Jimpitan RT.07 Randuares</h3>
     <script>
       document.addEventListener('DOMContentLoaded', function() {
